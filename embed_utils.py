f75d4ae4d6481bf579c6b626f472715eb268a32b
Luke Tchang <ltchang@stanford.edu>
2024-10-06 13:11:54 -0700

    load in fp16 and put on gpu

--- embed_utils.py
+++ embed_utils.py
@@ -1,11 +1,17 @@
+import torch
 from sentence_transformers import SentenceTransformer
 
 def get_embedding_model(model_name, max_seq_len=None):
     """
     Load the sentence transformer model and adjust the max_seq_length if provided.
     """
-    model = SentenceTransformer(model_name, trust_remote_code=True)
+    model = SentenceTransformer(
+        model_name, 
+        trust_remote_code=True, 
+        device="cuda" if torch.cuda.is_available() else "cpu",
+        torch_dtype=torch.float16,
+    )
     if max_seq_len:
         model.max_seq_length = max_seq_len
-    embedding_dim = model.get_sentence_embedding_dimension()  # Automatically get the dimension
+    embedding_dim = model.get_sentence_embedding_dimension()
     return model, embedding_dim
