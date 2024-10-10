4ebbfd22f45ee0116b8a63f043191ff214fc4f0c
Luke Tchang <ltchang@stanford.edu>
2024-10-09 20:33:47 -0700

    remove test load script

--- test_load.py
+++
@@ -1,6 +0,0 @@
-from data_utils import load_hits_from_qrels_queries_corpus
-
-# results = load_hits_from_qrels_queries_corpus("data/nq/nq_qrels.tsv", "data/nq/nq_queries.jsonl")
-results = load_hits_from_qrels_queries_corpus("data/msmarco/qrels/train.tsv", "data/msmarco/queries.jsonl")
-print(results)
-print("len", len(results))
