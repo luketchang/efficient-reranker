ff77a60be627054b593d0561fb56be08e86ac57f
Luke Tchang <ltchang@stanford.edu>
2024-10-16 12:39:19 -0700

    save teacher triples prog

--- datasets/teacher_triples.py
+++ datasets/teacher_triples.py
@@ -11,6 +11,15 @@
 # load rank results as qid --> pid --> score
 # load ground truth as qid --> pid --> score
 
+# get top 1000 embedding rank results (DONE)
+# send all embedding rank results to reranker (DONE)
+# remove false negatives (within 95% of lowest ground truth rerank score) (DONE)
+
+# need script that loads all query<>positive scores and sends to reranker for score (TODO)
+# pass positive qid->pid->score to dataset so it can put positive score onto rank result (TODO)
+
+# NOTE: we don't remove false negatives from rerank stage because we may still want to observe their behavior when sent through reranker or measure scoring
+
 from torch.utils.data import Dataset
 from data_utils import load_qid_to_pid_to_score, load_pids_to_passages, load_hits_from_qrels_queries_corpus
 
@@ -18,32 +27,42 @@ class TeacherTriplesDataset(Dataset):
     def __init__(self, queries_path, corpus_path, negative_rank_results_path, ground_truth_path):
         self.ground_truth = load_qid_to_pid_to_score(ground_truth_path)
         self.corpus = load_pids_to_passages(corpus_path)
-        negative_rank_results = load_hits_from_qrels_queries_corpus(negative_rank_results_path, queries_path, corpus_path)
+        rank_results = load_hits_from_qrels_queries_corpus(negative_rank_results_path, queries_path, corpus_path)
 
-        negative_rank_results_with_positives = []
-        for qid, hits in negative_rank_results.items():
+        self.negative_rank_results_with_positives = []
+        for rank_results in rank_results:
+            hits = rank_results['hits']
+            qid = hits[0]['qid']
             if qid in self.ground_truth:
                 for positive_id in self.ground_truth[qid]:
-                    negative_rank_results_with_positives.append({
-                        "query": hits['query'],
+                    self.negative_rank_results_with_positives.append({
                         "query_id": qid,
+                        "query": rank_results['query'],
                         "positive_id": positive_id,
-                        "hits": hits['hits']
+                        # "positive_score": "TODO",
+                        "hits": hits
                     })
-        self.negative_rank_results_with_positives = negative_rank_results_with_positives
-        
+
+        # Create index mapping: [(query_idx, hit_idx)]
+        self.index_mapping = []
+        for query_idx, rank_result in enumerate(self.negative_rank_results_with_positives):
+            num_hits = len(rank_result['hits'])
+            self.index_mapping.extend([(query_idx, hit_idx) for hit_idx in range(num_hits)])
+
     def __len__(self):
-        return sum(len(positive_with_negatives['hits']) for positive_with_negatives in self.negative_rank_results_with_positives)
-    
+        return len(self.index_mapping)
+
     def __getitem__(self, idx):
-        outer_idx = idx // len(self.negative_rank_results_with_positives)
-        hit_idx = idx % len(self.negative_rank_results_with_positives)
-        rank_result = self.negative_rank_results_with_positives[outer_idx]
+        query_idx, hit_idx = self.index_mapping[idx]
+        rank_result = self.negative_rank_results_with_positives[query_idx]
         query = rank_result['query']
         hit = rank_result['hits'][hit_idx]
         return {
             "query": query,
+            "positive_id": rank_result['positive_id'],
             "positive": self.corpus[rank_result['positive_id']],
-            "negative": hit['content']
+            # TODO: positive_score
+            "negative_id": hit['docid'],
+            "negative": hit['content'],
+            "negative_score": hit['score']
         }
-    
