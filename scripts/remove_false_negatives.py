f61a0148084e10473aee58ebad0a9da6c11332d4
Luke Tchang <ltchang@stanford.edu>
2024-10-17 22:36:33 -0700

    use positives score not taking from gt --> passage rankings

--- scripts/remove_false_negatives.py
+++ scripts/remove_false_negatives.py
@@ -1,18 +1,18 @@
 from data_utils import load_qid_to_pid_to_score
 import argparse
 
-def main(rank_results_path, ground_truth_path, top_k_perc=0.95, remove_positives=False):
+def main(rank_results_path, positives_path, top_k_perc=0.95, remove_positives=False):
     hits = load_qid_to_pid_to_score(rank_results_path)
-    ground_truth_labels = load_qid_to_pid_to_score(ground_truth_path)
+    positive_scores = load_qid_to_pid_to_score(positives_path)
     
     gt_pids = {}  # qid --> set of ground truth pids
     gt_min_scores = {}  # qid --> minimum score among ground truth pids
 
-    for qid, pid_to_score in ground_truth_labels.items():
+    for qid, pid_to_score in positive_scores.items():
         gt_pids[qid] = set(pid_to_score.keys())
         min_score = float('inf')
         for pid in gt_pids[qid]:
-            score = hits[qid].get(pid, None)
+            score = positive_scores[qid].get(pid, None)
             if score is not None and score < min_score:
                 min_score = score
         gt_min_scores[qid] = min_score
@@ -40,12 +40,13 @@ def main(rank_results_path, ground_truth_path, top_k_perc=0.95, remove_positives
 
     print(f"Filtered hits saved to {output_path}")
     print(f"Deleted {deleted_count} hits")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Remove false negatives from hits.")
     parser.add_argument("--rank_results_path", required=True, help="Path to the qrels file")
-    parser.add_argument("--ground_truth_path", required=True, help="Path to the ground truth file")
+    parser.add_argument("--positives_path", required=True, help="Path to the ground truth file")
     parser.add_argument("--top_k_perc", type=float, default=0.95, help="Percentage of the minimum ground truth score to keep")
     parser.add_argument("--remove_positives", action='store_true', default=False, help="Flag to remove positives")
     
     args = parser.parse_args()
-    main(args.rank_results_path, args.ground_truth_path, top_k_perc=args.top_k_perc, remove_positives=args.remove_positives)
+    main(args.rank_results_path, args.positives_path, top_k_perc=args.top_k_perc, remove_positives=args.remove_positives)
