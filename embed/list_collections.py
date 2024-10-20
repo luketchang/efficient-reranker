a86655c21039549555ae5b5bc4717956f8aac0e1
Luke Tchang <luketchang00@gmail.com>
2024-10-20 21:22:22 +0000

    add list collections script

---
+++ embed/list_collections.py
@@ -0,0 +1,4 @@
+from pymilvus import MilvusClient, connections
+connections.connect(host='127.0.0.1', port='19530')
+client = MilvusClient(f"http://127.0.0.1:19530")
+print(client.list_collections())
