4a3d647aa6ef9aa6adfa806d608bd82bf3687d73
Luke Tchang <luketchang00@gmail.com>
2024-10-30 20:41:22 +0000

    update rerank sh

--- rerank/run_rerank.sh
+++ rerank/run_rerank.sh
@@ -11,14 +11,15 @@ echo "Current working directory: $PWD"
 echo "Python path: $PYTHONPATH"
 
 PYTHONPATH=$PWD python rerank/rerank.py \
-    --model_name sentence-transformers/paraphrase-MiniLM-L6-v2 \
-    --rank_results_path data/fiqa/bge_en_icl_all_1000.tsv \
-    --qrels_path data/fiqa/qrels/test.tsv \
-    --queries_path data/fiqa/queries.jsonl \
-    --corpus_path data/fiqa/corpus.jsonl \
-    --output_path data/fiqa/paraphrase_info_nce_reranked_top_100.tsv \
+    --model_name microsoft/deberta-v3-large \
+    --checkpoint_path new-microsoft/deberta-v3-large-step-10751-inference.pth \
+    --rank_results_path data/hotpotqa/bge_en_icl_all_1000.tsv \
+    --qrels_path data/hotpotqa/qrels/test.tsv \
+    --queries_path data/hotpotqa/queries.jsonl \
+    --corpus_path data/hotpotqa/corpus.jsonl \
+    --output_path data/hotpotqa/deberta_margin_mse_reranked_top_100.tsv \
     --hits_per_query 100 \
     --flush_interval 32 \
-    --qid_base 10
+    --qid_base 16
 
 echo "Script finished at $(date)"
